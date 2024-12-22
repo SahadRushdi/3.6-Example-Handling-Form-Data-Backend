3cbc61f9fc4f65f1f9621c83f6dd102c266a3a04
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-12-22 10:18:26 +0530

    Crearted Add Products API.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var db = require("./database.js");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Enabling CORS
 const cors = require("cors");
@@ -19,18 +23,18 @@ app.listen(HTTP_PORT, () => {
   console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT));
 });
 
-//HTTP GET method
+// HTTP GET method
 app.get("/api/products", (req, res, next) => {
   try {
     // SQL query to Select all Data
-    var sql = "Select * from products";
+    var sql = "SELECT * FROM products";
     var params = [];
 
     // Running the SQL query
     db.all(sql, params, (err, rows) => {
       // Error Response
       if (err) {
-        res.status(400).json({error : err.message});
+        res.status(400).json({ error: err.message });
         return;
       }
 
@@ -40,13 +44,47 @@ app.get("/api/products", (req, res, next) => {
         data: rows,
       });
     });
-
   } catch (E) {
     res.status(400).send(E);
   }
 });
 
-//HTTP POST method
+// HTTP POST method
 app.post("/api/products", (req, res, next) => {
-  
+  try {
+    const { productName, description, unitPrice } = req.body;
+
+    // Validate input data
+    if (!productName || !description || !unitPrice) {
+      res.status(400).json({ error: "Missing required fields" });
+      return;
+    }
+
+    // SQL query to insert data into the database
+    var sql =
+      "INSERT INTO products (productName, description, unitPrice) VALUES (?,?,?)";
+    var params = [productName, description, unitPrice];
+
+    // Running the SQL query
+    db.run(sql, params, function (err) {
+      // Error Response
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+
+      // Success Response
+      res.status(200).json({
+        message: "Success",
+        data: {
+          id: this.lastID, // Return the ID of the inserted row
+          productName,
+          description,
+          unitPrice,
+        },
+      });
+    });
+  } catch (E) {
+    res.status(400).send(E);
+  }
 });
