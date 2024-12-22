91061cc9ed9178dba2d5d9b3d26cef10afd8bf70
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-12-22 09:34:25 +0530

    Added get Data API.

--- server.js
+++ server.js
@@ -11,7 +11,29 @@ app.listen(HTTP_PORT, () => {
 
 //HTTP GET method
 app.get("/api/products", (req, res, next) => {
-  
+  try {
+    // SQL query to Select all Data
+    var sql = "Select * from products";
+    var params = [];
+
+    // Running the SQL query
+    db.all(sql, params, (err, rows) => {
+      // Error Response
+      if (err) {
+        res.status(400).json({error : err.message});
+        return;
+      }
+
+      // Success Response
+      res.status(200).json({
+        message: "Success",
+        data: rows,
+      });
+    });
+
+  } catch (E) {
+    res.status(400).send(E);
+  }
 });
 
 //HTTP POST method
